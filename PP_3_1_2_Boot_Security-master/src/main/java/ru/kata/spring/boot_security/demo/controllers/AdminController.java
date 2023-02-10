91c6cd7086afb51eec1347af3cfe27d3079abf5a
Oleona <Ole-onna@mail.ru>
2023-02-10 03:17:13 +0300

    Added check - whether the user exists in the database - when creating

--- PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -2,12 +2,13 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
 
 import java.util.List;
 
@@ -16,10 +17,14 @@ import java.util.List;
 public class AdminController {
     private final UserService userService;
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
-    public AdminController(UserService userService, RoleRepository roleRepository) {
+
+    public AdminController(UserService userService, RoleRepository roleRepository, UserRepository userRepository) {
         this.userService = userService;
         this.roleRepository = roleRepository;
+
+        this.userRepository = userRepository;
     }
 
 
@@ -37,29 +42,9 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-//    @GetMapping(value = "/admin/new")
-//    public ModelAndView newUser() {
-//        User user = new User();
-//        ModelAndView mav = new ModelAndView("/admin/new");
-//        mav.addObject("user", user);
-//
-//        List<Role> roles = (List<Role>) roleRepository.findAll();
-//
-//        mav.addObject("allRoles", roles);
-//
-//        return mav;
-//    }
-//    @PostMapping(value = "/admin/new")
-//    public String createNewUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return "/admin/new";
-//        }
-//        userService.add(user);
-//        return "redirect:/admin";
-//    }
 
     @GetMapping(value = "/admin/new")
-    public String newEmptyUser(@ModelAttribute("user") User user,ModelMap model) {
+    public String newEmptyUser(@ModelAttribute("user") User user, ModelMap model) {
         List<Role> roles = (List<Role>) roleRepository.findAll();
         model.addAttribute("allRoles", roles);
         return "/new";
@@ -67,19 +52,14 @@ public class AdminController {
 
 
     @PostMapping()
-    public String createNewUser(@ModelAttribute("user")  User user) {
-//        if (bindingResult.hasErrors()) {
-//            return "/admin/new";
-//        }
-      //  List<User> users = userService.listUsers().stream().toList();
-       // var a=users.contains(user.getUsername());
-      //  if(!a){
+    public String createNewUser(@ModelAttribute("user") User user) {
+        if (userRepository.findByUsername(user.getUsername()) == null) {
             userService.add(user);
             return "redirect:/admin";
-      //  }
-//      else{
-//            return "/admin/new";
-//        }
+        } else {
+            return "/new";
+        }
+
 
     }
 
@@ -92,20 +72,14 @@ public class AdminController {
     }
 
     @PatchMapping("/admin/edit/{id}")
-    public String update(@ModelAttribute("user")  User user,  @PathVariable("id") Long id) {
-//        if (bindingResult.hasErrors()) {
-//            return "/edit";
-//        }
-        userService.update(user,id);
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+
+        userService.update(user, id);
         return "redirect:/admin";
     }
-//@GetMapping(value = "/admin/new")
-//public String Users(ModelMap model) {
-//    System.out.println("This");
-//    return "/new";
-//}
-@GetMapping()
-public String getstart() {
-    return "index";
-}
+
+    @GetMapping()
+    public String getstart() {
+        return "index";
+    }
 }
